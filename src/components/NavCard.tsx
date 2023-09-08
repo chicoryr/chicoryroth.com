48083bac388c65d0cbdc9cd86190242c49b8443c
Chicory Roth <chicoryroth@gmail.com>
2023-09-08 11:44:39 -0700

    fixing mobile pdf hopefully

--- src/components/NavCard.tsx
+++ src/components/NavCard.tsx
@@ -4,7 +4,7 @@ export default function NavCard
 () {
   return (
     <div className="bg-gray-500 rounded-lg p-5">
-      <div className="text-white text-center mb-1">Full-stack Software Engineer</div>
+      <div className="text-black text-center mb-1">Full-stack Software Engineer</div>
       <Links/>
     </div>
   )
--- src/components/ResumeCard.tsx
+++ src/components/ResumeCard.tsx
@@ -1,13 +1,10 @@
-
+const pdfUrl = "/resume.pdf"
 export default function ResumeCard() {
   return (
-    <div className="grid h-screen place-items-center bg-black">
-      <embed
-	src="resume.pdf"
-	type="application/pdf"
-	width="50%"
-	height="100%"
-/>
+    <div className="grid h-screen place-items-center bg-white">
+      <iframe src={pdfUrl} width="100%" height="100%">
+    This browser does not support inline PDFs. Please download the PDF to view it: <a href={pdfUrl}>Download PDF</a>
+</iframe>
     </div>
   )
 };
