20187e7510cf0f60711a132268ee09dcf9467f56
Chicory Roth <chicoryroth@gmail.com>
2023-09-07 20:37:27 -0700

    forgot files tsk tsk

---
+++ src/components/ResumeCard.tsx
@@ -0,0 +1,13 @@
+
+export default function ResumeCard() {
+  return (
+    <div className="grid h-screen place-items-center">
+      <embed
+	src="resume.pdf"
+	type="application/pdf"
+	width="50%"
+	height="100%"
+/>
+    </div>
+  )
+};
