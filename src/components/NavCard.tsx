29448d86f7b96cde3bbff5b495b21551409f69c0
Chicory Roth <chicoryroth@gmail.com>
2025-08-05 19:59:06 -0700

    font size changes

--- src/components/NavCard.tsx
+++ src/components/NavCard.tsx
@@ -4,7 +4,7 @@ export default function NavCard
 () {
   return (
     <div className="bg-gray-500 rounded-lg p-5">
-      <div className="text-black text-center mb-1 text-xl font-bold font-sans">Software Engineer</div>
+      <div className="text-black text-center mb-1 text-lg font-bold font-sans">Software Engineer</div>
       <Links/>
     </div>
   )
