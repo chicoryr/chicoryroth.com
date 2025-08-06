ded6517ef7b665a8c92d8c377330200cec8e0eaf
Chicory Roth <chicoryroth@gmail.com>
2025-08-05 21:19:58 -0700

    simplified links

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -8,7 +8,7 @@ export default function Links() {
       {LINKS.map((link, i) =>{
         return (
             <a 
-              className="group flex flex-col items-center p-2.5 sm:p-3 rounded-lg hover:bg-gray-100 transition-all duration-300 min-w-0 flex-shrink-0"
+              className="group flex flex-col items-center p-0 sm:p-3 rounded-lg hover:bg-gray-100 transition-all duration-300 min-w-0 flex-shrink-0"
               key={i}
               target={link.newTab ? "_blank" : ""}
               href={link.url}
@@ -31,7 +31,7 @@ export default function Links() {
 
 const LINKS = [
   {
-      url: "https://www.linkedin.com/in/chicory-roth/",
+      url: "https://www.li nkedin.com/in/chicory-roth/",
       text: "LinkedIn",
       newTab: true,
       icon: <svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" viewBox="0 0 1024 1024"><path fill="currentColor" d="M847.7 112H176.3c-35.5 0-64.3 28.8-64.3 64.3v671.4c0 35.5 28.8 64.3 64.3 64.3h671.4c35.5 0 64.3-28.8 64.3-64.3V176.3c0-35.5-28.8-64.3-64.3-64.3zm0 736c-447.8-.1-671.7-.2-671.7-.3c.1-447.8.2-671.7.3-671.7c447.8.1 671.7.2 671.7.3c-.1 447.8-.2 671.7-.3 671.7zM230.6 411.9h118.7v381.8H230.6zm59.4-52.2c37.9 0 68.8-30.8 68.8-68.8a68.8 68.8 0 1 0-137.6 0c-.1 38 30.7 68.8 68.8 68.8zm252.3 245.1c0-49.8 9.5-98 71.2-98c60.8 0 61.7 56.9 61.7 101.2v185.7h118.6V584.3c0-102.8-22.2-181.9-142.3-181.9c-57.7 0-96.4 31.7-112.3 61.7h-1.6v-52.2H423.7v381.8h118.6V604.8z"/></svg>
